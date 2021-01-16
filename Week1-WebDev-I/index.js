c58af947d0cd4c6d3f08e3c23613be51f001ecc6
Jason Zhang <zhang98@bu.edu>
2021-01-15 21:18:07 -0500

    counter example for week 1

---
+++ Week1-WebDev-I/index.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+
+let count = 0;
+
+const countHeader = document.getElementById('counter')
+
+countHeader.innerHTML = 'Counter: ' + count
+
+const addOne = () =>
+{
+    count = count + 1
+
+    countHeader.innerHTML = 'Counter: ' + count
+}
+const minusOne = () =>
+{
+    count = count - 1
+
+    countHeader.innerHTML = 'Counter: ' + count
+}
+const reset = () =>
+{
+    count = 0
+
+    countHeader.innerHTML = 'Counter: ' + count
+}
