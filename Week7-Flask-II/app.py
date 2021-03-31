484b27099fca59e57b7dbe785063ac0a9be73bdd
jzhang0107 <zhang98@bu.edu>
2021-03-31 18:24:25 -0400

    week 7 api request challenge complete

--- Week7-Flask-II/app.py
+++ Week7-Flask-II/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import requests
+
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
@@ -8,5 +10,20 @@ def helloWorld():
 @app.route('/formSubmit', methods=["POST"])
 def formSubmit():
     firstName = request.form.get("fname")
-    pokemonName = request.form.get("pokemonName")
-    return render_template('result.html', fname = firstName, pokemonName = pokemonName)
+    uniName = request.form.get("uniName")
+
+    url = "http://universities.hipolabs.com/search?"
+    payload = {'name': uniName}
+
+    response = requests.get(url, params = payload)
+    print(response.url)
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(response.headers['content-type'])
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(response.content)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(response.text)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    return render_template('result.html', fname = firstName, uniName = uniName, uniContent = response.text)
